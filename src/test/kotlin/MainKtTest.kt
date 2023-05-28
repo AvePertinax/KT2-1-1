10c27e728febc7ed1b063f75fc55b929636438cb
Vlad Gromov <vladoffrevan@mail.ru>
2023-05-28 20:17:32 +0300

    return stable

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -8,14 +8,14 @@ class MainKtTest {
     fun calcPaymentCommissionDefault() {
         assertEquals(calcPaymentCommission(10), 0)
         assertEquals(calcPaymentCommission(100), 0)
-        assertEquals(calcPaymentCommission(1000), 10)
+        assertEquals(calcPaymentCommission(1000), 0)
         assertEquals(calcPaymentCommission(10000), 0)
         assertEquals(calcPaymentCommission(100000), 0)
     }
 
     @Test
     fun calcPaymentCommissionVisaMir() {
-        assertEquals(calcPaymentCommission(1000, PaymentSystem.VISA), 75)
+        assertEquals(calcPaymentCommission(1000, PaymentSystem.VISA), 35)
         assertEquals(calcPaymentCommission(10000, PaymentSystem.VISA), 75)
     }
 
@@ -24,7 +24,7 @@ class MainKtTest {
         assertEquals(calcPaymentCommission(100, PaymentSystem.MAESTRO, 1000), 20)
         assertEquals(calcPaymentCommission(100, PaymentSystem.MAESTRO, 10000), 20)
         assertEquals(calcPaymentCommission(100, PaymentSystem.MAESTRO, 80000), 20)
-        assertEquals(calcPaymentCommission(1000, PaymentSystem.MAESTRO, 1000), 100)
+        assertEquals(calcPaymentCommission(1000, PaymentSystem.MAESTRO, 1000), 0)
         assertEquals(calcPaymentCommission(1000, PaymentSystem.MAESTRO, 10000), 0)
         assertEquals(calcPaymentCommission(1000, PaymentSystem.MAESTRO, 80000), 26)
     }
